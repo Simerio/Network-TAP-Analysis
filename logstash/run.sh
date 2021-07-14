95fd34e3a0e655ec15b86d2979edf72d6f8a3b8e
Simo <codesimo@gmail.com>
2021-07-15 00:31:36 +0200

    fix(logstash): edit run.sh

--- logstash/run.sh
+++ logstash/run.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-set -x
 
-if [[ $(/usr/bin/id -u) -ne 0 ]]; then 
+if [ $(/usr/bin/id -u) -ne 0 ]; then 
     echo "Not running as root"     
     exit 
 fi
+
+set -x
 
 tshark -i wlo1 -lT ek -lT fields -E separator=, -E quote=d -e frame.time_epoch -e ip.src -e ip.dst_host -e tcp.dstport -Y "ip.dst != 192.168.1.3 && tcp " > ./pcap/pcap.csv
