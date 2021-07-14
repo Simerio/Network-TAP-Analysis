52686f7c530789b016f6505444c053cb4d07d115
cata981 <cata98@live.it>
2021-07-14 23:37:58 +0200

    feat(logstash): add filter

--- logstash/run.sh
+++ logstash/run.sh
@@ -7,5 +7,4 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     exit 
 fi
 
-#tshark -i wlo1 -lT ek -lT fields -E separator=, -E quote=d -e frame.time_epoch -e ip.src -e ip.dst_host -e tcp.dstport -e udp.dstport -Y "ip.dst != 192.168.1.3 && tcp " > ./pcap/pcap.json
-tshark -i wlo1 -lT json -Y "ip.dst != 192.168.1.3" -N n > pcap.json
+tshark -i wlo1 -lT ek -lT fields -E separator=, -E quote=d -e frame.time_epoch -e ip.src -e ip.dst_host -e tcp.dstport -Y "ip.dst != 192.168.1.3 && tcp " > ./pcap/pcap.csv
