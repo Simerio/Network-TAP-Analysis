499098bc7fecbdb75cc03cc150dfdfe3bf199b3d
cata981 <cata98@live.it>
2021-07-18 21:50:13 +0200

    feat(spark) add mapping

--- spark/spark.py
+++ spark/spark.py
@@ -8,40 +8,42 @@ import pyspark.sql.types as tp
 
 
 kafkaServer = "kafkaserver:9092"
-elastic_host="elasticsearch"
+elastic_host="http://elasticsearch"
 
 elastic_topic = "tap"
 elastic_index = "tap"
 
-# es_mapping = {
-#     "mappings": {
-#         "properties": 
-#             {
-#                 "@timestamp": {"type": "date"},
-#                 "hostname": {"type": "text"},
-#                 "ip_src":{"type":"ip"},
-#                 "port":{"type":"integer"},
-#                 "geoip":{
-#                     "ip": { "type": "ip" },
-#                     "location" : { "type" : "geo_point" },
-#                     "latitude" : { "type" : "half_float" },
-#                     "longitude" : { "type" : "half_float" },
-#                     "country_name":{"type": "text"},
-#                     "continent_code" : {"type": "text"},
-#                     "country_code2" : {"type": "text"}
-#                 }
-#             }
-#     }
-# }
+es_mapping = {
+    "mappings": {
+        "properties": 
+            {
+                "@timestamp": {"type": "date"},
+                "hostname": {"type": "text"},
+                "ip_src":{"type":"ip"},
+                "port":{"type":"integer"},
+                "geoip":{
+                    "ip": { "type": "ip" },
+                    "location" : { "type" : "geo_point" },
+                    "country_name":{"type": "text"},
+                    "continent_code" : {"type": "text"},
+                    "country_code2" : {"type": "text"},
+                    "dma_code": {"type":"integer"},
+                    "region_name": {"type":"text"},
+                    "city_name": {"type":"text"}
+                }
+            }
+    }
+}
 
-# es = Elasticsearch(hosts=elastic_host) 
-# # make an API call to the Elasticsearch cluster
-# # and have it return a response:
-# response = es.indices.create(
-#     index=elastic_index,
-#     body=es_mapping,
-#     ignore=400 # ignore 400 already exists code
-# )
+
+es = Elasticsearch(hosts=elastic_host) 
+# make an API call to the Elasticsearch cluster
+# and have it return a response:
+response = es.indices.create(
+    index=elastic_index,
+    body=es_mapping,
+    ignore=400 # ignore 400 already exists code
+)
 
 sparkConf = SparkConf().set("spark.app.name", "network-tap") \
                         .set("spark.scheduler.mode", "FAIR") \
@@ -51,27 +53,7 @@ sparkConf = SparkConf().set("spark.app.name", "network-tap") \
 sc = SparkContext.getOrCreate(conf=sparkConf)
 spark = SparkSession(sc)
 
-'''
-{
-    "ip_src" => "172.20.218.206",
-    "geoip" => {
-        "country_code2" => "US",
-        "continent_code" => "NA",
-        "region_name" => "Virginia",
-        "location" => {
-            "lon" => -78.375,
-            "lat" => 36.6534
-        },
-        "dma_code" => 560,
-        "ip" => "65.55.44.109",
-        "country_name" => "United States",
-        "city_name" => "Boydton"
-    },
-    "@timestamp" => 2021-07-17T16:55:40.025Z,
-    "port" => 443,
-    "hostname" => "65.55.44.109"
-}
-'''
+
 
 df_kafka = spark \
     .readStream \
@@ -81,16 +63,33 @@ df_kafka = spark \
     .load()
 
 network_tap = tp.StructType([
-    tp.StructField(name= 'id_str', dataType= tp.StringType(),  nullable= True),
-    tp.StructField(name= 'created_at', dataType= tp.StringType(),  nullable= True),
-    tp.StructField(name= 'text',       dataType= tp.StringType(),  nullable= True)
+    tp.StructField(name= '@timestamp', dataType= tp.DateType(),  nullable= True),
+    tp.StructField(name= 'hostname', dataType= tp.StringType(),  nullable= True),
+    tp.StructField(name= 'ip_src', dataType= tp.StringType(),  nullable= True),
+    tp.StructField(name= 'port', dataType= tp.IntegerType(),  nullable= True),
+    tp.StructField(name= 'ip', dataType= tp.StringType(),  nullable= True),
+    tp.StructField(name= 'location', dataType= tp.StringType(),  nullable= True),
+    tp.StructField(name= 'country_name', dataType= tp.StringType(),  nullable= True),
+    tp.StructField(name= 'continent_code', dataType= tp.StringType(),  nullable= True),
+    tp.StructField(name= 'country_code2', dataType= tp.StringType(),  nullable= True),
+    tp.StructField(name= 'dma_code', dataType= tp.IntegerType(),  nullable= True),
+    tp.StructField(name= 'region_name', dataType= tp.StringType(),  nullable= True),
+    tp.StructField(name= 'city_name', dataType= tp.StringType(),  nullable= True)
 ])
+
+
 
 
 df_elastic = df_kafka.selectExpr("CAST(value AS STRING)") \
     .select(from_json("value", network_tap).alias("data")) \
-    .select("data.*")
+    .select("data.*") \
+    .writeStream \
+    .format("es") \
+    .option("es.nodes", elastic_host).save(elastic_index) \
+    .start() \
+    .awaitTermination() 
 
+'''
 #TODO: importare Elasticsearch e fare i mapping. guarda esempio twitter.
 
 query_elastic = df_elastic.writeStream \
@@ -101,3 +100,4 @@ query_elastic = df_elastic.writeStream \
         
 # Keep running untill terminated
 query_elastic.awaitTermination()
+'''
